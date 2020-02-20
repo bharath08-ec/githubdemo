c60b6ebadc488d809b94009f878891baddb26129
BLTuser <BLTuser@BLT1222>
2020-02-20 11:50:38 +0530

    firstcommit

---
+++ GradleDemo/src/test/java/BasicDemo.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BasicDemo {
+	@Test
+	public void testcase()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\BLTuser.BLT1222\\Desktop\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+	    driver.get("http://www.busindia.com/PRTC-Pondicherry-Online-Booking.jsp ");
+	    driver.manage().window().maximize();
+	}
+
+}
