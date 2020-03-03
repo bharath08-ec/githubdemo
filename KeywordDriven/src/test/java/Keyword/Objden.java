8a3471f2bc6666be64a72c5e589a75b9c3121500
BLTuser <BLTuser@BLT1222>
2020-03-03 09:55:20 +0530

    Keyword driven

---
+++ KeywordDriven/src/test/java/Keyword/Objden.java
@@ -0,0 +1,35 @@
+package Keyword;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Objden {
+	   
+	
+		WebDriver driver;
+		By searchword=By.name("q");
+		
+		public void OpenBrow()
+		{
+			
+			System.setProperty("webdriver.chrome.driver","C:\\\\Users\\\\BLTuser.BLT1222\\\\Desktop\\\\chromedriver.exe");
+			driver=new ChromeDriver();
+
+		}
+	    public void SetUrl()
+	    {
+	    	driver.manage().window().maximize();
+			driver.get("https://google.com");
+	    }
+	    
+	    public void Search(String st)
+	    {
+	    driver.findElement(searchword).sendKeys(st);	
+	    	
+	    }
+	    public void closeBrow()
+	    {
+	    	driver.close();
+	    }
+}
